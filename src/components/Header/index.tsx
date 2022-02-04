ccd2e8c526381afcfe5617b401fb2044114c0fb3
matheuskarnas <matheuskarnas1@gmail.com>
2022-02-04 02:51:51 -0300

    testing @mui/icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Slider, Stack } from "@mui/material";
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
 
-export const Header = () => 
-    <Box height="5rem">
-        Header
-    </Box>
+export const Header = () => {
+    
+    return (
+        <>
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                <VolumeDown />
+                <Slider aria-label="Volume" value={2}  />
+                <VolumeUp />
+            </Stack>
+            <Slider  defaultValue={30} aria-label="Disabled slider" />
+        </>
+    )
+}
