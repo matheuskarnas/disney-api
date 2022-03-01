3dde2c58fe66aa7006ce2c7c4c06748014ae7ed6
matheuskarnas <matheuskarnas1@gmail.com>
2022-02-28 23:43:56 -0300

    stylized modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,11 +7,23 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  minWidth: 350,
+  bgcolor: '#35313f',
   boxShadow: 24,
   p: 4,
+  textAlign: 'center',
+  borderRadius: '4rem',
+
+  h2: {
+  },
+
+  h3: {
+    fontSize: '1rem',
+    span: {
+      color: '#000000ad',
+      fontSize: '0.95rem'
+    },
+  }
 };
 
 type ModalProps = {
@@ -31,38 +43,37 @@ export const CharacterModal = ({ character, handleClose, isOpen }: ModalProps) =
     >
       <Box sx={style} color="primary.main">
         <img src={character.imageUrl} />
-        <h3>{`name: ${character.name}` || ''}</h3>
+        <h2>{`${character.name}` || ''}</h2>
 
         {character.allies.length > 0
-          ? <h3>{`Aliados: ${character.allies}`}</h3>
+          ? <h3> <span>Aliados &nbsp;</span> {character.allies} </h3>
           : null
         }
 
         {character.enemies.length > 0
-          ? <h3>{`inimigos: ${character.enemies}`}</h3>
+          ? <h3> <span>Inimigos &nbsp;</span> {character.enemies} </h3>
           : null
         }
 
         {character.films.length > 0
-          ? <h3>{`Filmes: ${character.films}`}</h3>
+          ? <h3> <span>Filmes &nbsp;</span> {character.films} </h3>
           : null
         }
 
         {character.shortFilms.length > 0
-          ? <h3>{`Curta metragem: ${character.shortFilms}`}</h3>
+          ? <h3> <span>Curtas-metragens &nbsp;</span> {character.shortFilms} </h3>
           : null
         }
 
         {character.tvShows.length > 0
-          ? <h3>{`programas de TV: ${character.tvShows}`}</h3>
+          ? <h3> <span>programas de TV &nbsp;</span> {character.tvShows} </h3>
           : null
         }
 
         {character.videoGames.length > 0
-          ? <h3>{`Videogames: ${character.videoGames}`}</h3>
+          ? <h3> <span>Videogames &nbsp;</span> {character.videoGames} </h3>
           : null
         }
-
       </Box>
     </Modal>
   </div>
