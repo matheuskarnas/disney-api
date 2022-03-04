6f303ef6eaa32dc23c5bda91e45562827a1e1c29
matheuskarnas <matheuskarnas1@gmail.com>
2022-03-04 07:27:03 -0300

    upgrade modal css

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import { blueGrey, grey } from '@mui/material/colors';
 import Modal from '@mui/material/Modal';
 import { AllDataOfCharters } from '../App'
 
@@ -8,21 +9,35 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   minWidth: 350,
+  maxHeight: 600,
   bgcolor: '#35313f',
   boxShadow: 24,
-  p: 4,
+  p: 1,
   textAlign: 'center',
-  borderRadius: '4rem',
+  borderRadius: '1rem',
   border: 'none',
 
+  img: {
+    maxHeight: '300px',
+    borderRadius: '0.5rem',
+    width: '100%',
+  },
+
+  h2: {
+    marginTop: '0.25rem',
+    color: grey[50],
+  },
+
   h3: {
+    textAlign: 'left',
     fontSize: '1rem',
+    fontWeight: 'lighter',
     span: {
-      color: '#000000ad',
-      fontSize: '0.95rem'
+      color: blueGrey[100],
     },
   }
-};
+
+}
 
 type ModalProps = {
   isOpen: boolean,
@@ -32,7 +47,6 @@ type ModalProps = {
 
 export const CharacterModal = ({ character, handleClose, isOpen }: ModalProps) => (
   <div>
-
     <Modal
       open={isOpen}
       onClose={handleClose}
@@ -48,32 +62,32 @@ export const CharacterModal = ({ character, handleClose, isOpen }: ModalProps) =
         <h2>{`${character.name}` || ''}</h2>
 
         {character.allies.length > 0
-          ? <h3> <span>Aliados &nbsp;</span> {character.allies} </h3>
+          ? <h3> <span>Aliados : &nbsp;</span> {character.allies}.</h3>
           : null
         }
 
         {character.enemies.length > 0
-          ? <h3> <span>Inimigos &nbsp;</span> {character.enemies} </h3>
+          ? <h3> <span>Inimigos : &nbsp;</span> {character.enemies}.</h3>
           : null
         }
 
         {character.films.length > 0
-          ? <h3> <span>Filmes &nbsp;</span> {character.films} </h3>
+          ? <h3> <span>Filmes : &nbsp;</span> {character.films}.</h3>
           : null
         }
 
         {character.shortFilms.length > 0
-          ? <h3> <span>Curtas-metragens &nbsp;</span> {character.shortFilms} </h3>
+          ? <h3> <span>Curtas-metragens : &nbsp;</span> {character.shortFilms}.</h3>
           : null
         }
 
         {character.tvShows.length > 0
-          ? <h3> <span>programas de TV &nbsp;</span> {character.tvShows} </h3>
+          ? <h3> <span>Programas de TV : &nbsp;</span> {character.tvShows}.</h3>
           : null
         }
 
         {character.videoGames.length > 0
-          ? <h3> <span>Videogames &nbsp;</span> {character.videoGames} </h3>
+          ? <h3> <span>Videogames : &nbsp;</span> {character.videoGames}.</h3>
           : null
         }
       </Box>
