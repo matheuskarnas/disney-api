92150a0d47f6a5f8de37d6ec396b489783467c18
matheuskarnas <matheuskarnas1@gmail.com>
2023-09-11 16:00:18 -0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,13 @@
-import { Header } from './components/Header';
-import { ListOfCharacters } from './components/ListOfCharacters';
-import { Box } from '@mui/material';
-import { useEffect } from 'react';
-import { useState } from "react"
-import { responseApi } from './services/apiCall';
-import { Footer } from './components/Footer';
-
-export type AllDataOfCharters = {
-  _id: number
-  name: string;
-  imageUrl: string;
-  url: string;
-  films?: string[];
-  shortFilms?: string[];
-  tvShows?: string[];
-  videoGames?: string[];
-  parkAttractions?: string[];
-  allies?: string[];
-  enemies?: string[];
-}
-
-export type ArrayOfCharactersProps = {
-  arrayCharacters: AllDataOfCharters[];
-}
-
-export const baseUrl = 'https://api.disneyapi.dev/characters?page=1'
-
+import { Header } from './components/Header'
+import { ListOfCharacters } from './components/ListOfCharacters'
+import { Box } from '@mui/material'
+import { Footer } from './components/Footer'
 
 export const App = () => {
-  const [characters, setCharacters] = useState<AllDataOfCharters[]>([])
-  const [validation, setValidation] = useState<string>(baseUrl)
-
-  useEffect(() => {
-    if (validation !== undefined) {
-      const teste = async () => {
-        const [data, nextPage] = await responseApi(validation)
-        setCharacters(prev => [...prev, ...data])
-        setValidation(nextPage)
-      }
-      teste()
-    }
-  }, [validation])
-
   return (
-    <Box
-      bgcolor='secondary.main'
-      color='primary.main'
-      minHeight='100vh'
-    >
+    <Box bgcolor='secondary.main' color='primary.main' minHeight='100vh'>
       <Header />
-      <ListOfCharacters characters={characters} />
+      <ListOfCharacters />
       <Footer />
     </Box>
   )
