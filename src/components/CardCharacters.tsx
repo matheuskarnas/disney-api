11194b46cba6982003d069888e06db56e7218e11
matheuskarnas <matheuskarnas1@gmail.com>
2023-09-11 15:59:18 -0300

    fix(indentation)

--- src/components/CardCharacters.tsx
+++ src/components/CardCharacters.tsx
@@ -1,28 +1,28 @@
-import { Box, Grid } from '@mui/material';
-import { useState } from 'react';
-import { AllDataOfCharters } from '../App';
-import { CharacterModal } from './Modal';
+import { Box, Grid } from '@mui/material'
+import { useState } from 'react'
+import { CharacterModal } from './Modal'
+import { Character } from '../types/types'
 
 type CardCharactersProps = {
-  character: AllDataOfCharters
+  character: Character
 }
 
 export const CardCharacters = ({ character }: CardCharactersProps) => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   return (
     <>
-      <CharacterModal character={character} handleClose={handleClose} isOpen={open} />
-      <Grid
-        onClick={() => handleOpen()}
-        item
-
-      >
+      <CharacterModal
+        character={character}
+        handleClose={handleClose}
+        isOpen={open}
+      />
+      <Grid onClick={() => handleOpen()} item>
         <Box
-          bgcolor="background.paper"
-          minWidth="300px"
+          bgcolor='background.paper'
+          minWidth='300px'
           height='20rem'
           textAlign='center'
           display='flex'
@@ -31,14 +31,17 @@ export const CardCharacters = ({ character }: CardCharactersProps) => {
             borderRadius: '12px',
             flexDirection: 'column',
             justifyContent: 'space-between'
-          }}
-        >
+          }}>
           <img
             src={character.imageUrl}
             alt={`personagem de nome ${character.name}`}
-            style={{ width: '100%', maxHeight: '15rem', borderRadius: ' 12px 12px 0 0' }}
+            style={{
+              width: '100%',
+              maxHeight: '15rem',
+              borderRadius: ' 12px 12px 0 0'
+            }}
           />
-          <h1 style={{ fontSize: '1.25rem' }} > {character.name} </h1>
+          <h1 style={{ fontSize: '1.25rem' }}> {character.name} </h1>
         </Box>
       </Grid>
     </>
